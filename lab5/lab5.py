48346c3ed1eb30df88030a7216480930073a2a26
Zachmau5 <123523690+Zachmau5@users.noreply.github.com>
2023-02-18 13:42:28 -0700

    Create lab5.py

---
+++ lab5/lab5.py
@@ -0,0 +1,24 @@
+import numpy as np
+import matplotlib.pyplot as plt
+
+def main():
+    tau = 0.001
+    t = np.linspace(0, 5*tau)
+    f_t = 5-5*np.e**(-t/tau)
+    plt.plot(t, f_t)
+    plt.xlabel("Time    [s]")
+    plt.ylabel(r"$V_{0}(t)$    [V]")
+    plt.title(r"$V_{0}(t)$ with respect to time")
+    plt.show()
+    print(find_time_constant(t, f_t))
+
+def find_time_constant(t,f_t):
+    vMax = max(f_t)
+    midPoint = 0.632*vMax
+    differenceArray = np.absolute(f_t - midPoint)
+    index = differenceArray.argmin()
+    tau = t[index]
+    return tau
+
+if __name__ == '__main__':
+    main()
