6a67ad08d7a27eddf9243410d037bb84e893974f
Zachmau5 <123523690+Zachmau5@users.noreply.github.com>
2023-04-21 11:33:11 -0600

    Update Bode Plot.py

--- Lab 9/Bode Plot.py
+++ Lab 9/Bode Plot.py
@@ -3,15 +3,14 @@ from scipy import signal
 
 def main():
         sys = signal.TransferFunction([2e7], [1, 2500, 2e7])
-        
         w, mag, phase = signal.bode(sys)
-        
-        plt.figure()
+        fig=plt.figure(figsize=(20,15))
+        fig.suptitle("High and Low Pass Bode Plots",fontsize=40)
+        plt.subplot(2,2,1)
         plt.semilogx(w, mag)
         plt.grid(True)
         plt.title("Low Pass Magnitude")
-        
-        plt.figure()
+        plt.subplot(2,2,2)
         plt.semilogx(w, phase)
         plt.grid(True)
         plt.title("Low Pass Angle")
@@ -20,17 +19,18 @@ def main():
 
         w, mag, phase = signal.bode(sys)
         
-        plt.figure()
+        plt.subplot(2,2,3)
         plt.semilogx(w, mag)
         plt.grid(True)
         plt.title("High Pass Magnitude")
 
-        plt.figure()
+        plt.subplot(2,2,4)
         plt.semilogx(w, phase)
         plt.grid(True)
-        plt.title("High Pass Angle")
-        
+        plt.title("High Pass Angle")      
+        plt.savefig("Bode Plot.png")
         plt.show()
+        
         
 if __name__ == "__main__":
     main()
